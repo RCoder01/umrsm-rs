38368bd70666823ef4e4559c8b04aca46ee5aa92
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2023-08-08 12:20:16 -0400

    Working state machine framework

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,105 @@
-fn main() {}
+fn main() {
+    let mut machine = StateMachine::default();
+    machine.add_state::<Start>();
+    machine.add_state::<Mid>();
+    machine.add_state::<Stop>();
+    let runner = machine
+        .runner::<Start>(0, ())
+        .expect("Start exists in machine");
+    dbg!(runner.run_to_completion());
+}
+
+use umrsm_rs::sm::{IntoOutcome, Outcome, OutcomeData, State, StateMachine};
+
+type Data = usize;
+
+#[derive(Default)]
+struct Start;
+
+impl State for Start {
+    type Income = ();
+
+    type Transition = OutcomeData<Mid>;
+
+    type Data = Data;
+
+    fn init(&mut self, _previous: Box<Self::Income>) {}
+
+    fn handle(&mut self, data: &mut Self::Data) -> Self::Transition {
+        println!("on start: {}", data);
+        OutcomeData::with_name(0, "StartTransition".to_string())
+    }
+
+    fn name(&self) -> String {
+        "Start".to_string()
+    }
+}
+
+enum MidOutcome {
+    Continue(<Mid as State>::Income),
+    End(usize),
+}
+
+impl IntoOutcome for MidOutcome {
+    fn into_outcome(self) -> Box<dyn Outcome> {
+        match self {
+            MidOutcome::Continue(d) => OutcomeData::<Mid>::with_name(
+                d,
+                "MidOutcome::Continue".to_string(),
+            )
+            .into_outcome(),
+            MidOutcome::End(v) => OutcomeData::<Stop>::with_name(
+                v.to_string(),
+                "MidOutcome::End".to_string(),
+            )
+            .into_outcome(),
+        }
+    }
+}
+
+#[derive(Default)]
+struct Mid(i32);
+
+impl State for Mid {
+    type Income = i32;
+    type Transition = MidOutcome;
+    type Data = Data;
+
+    fn init(&mut self, previous: Box<Self::Income>) {
+        self.0 = *previous;
+    }
+
+    fn handle(&mut self, data: &mut Self::Data) -> Self::Transition {
+        if *data > 10 {
+            MidOutcome::End(*data)
+        } else {
+            *data += 1;
+            MidOutcome::Continue(*data as i32 + 10000)
+        }
+    }
+
+    fn name(&self) -> String {
+        "Mid".to_string()
+    }
+}
+
+#[derive(Default)]
+struct Stop(String);
+
+impl State for Stop {
+    type Income = String;
+    type Transition = ();
+    type Data = Data;
+
+    fn init(&mut self, previous: Box<Self::Income>) {
+        self.0 = *previous;
+    }
+
+    fn handle(&mut self, _data: &mut Self::Data) -> Self::Transition {
+        println!("on end: {}", self.0);
+    }
+
+    fn name(&self) -> String {
+        "Stop".to_string()
+    }
+}
--- src/sm.rs
+++ src/sm.rs
@@ -1,88 +1,281 @@
-use std::any::{Any, TypeId};
+use std::{
+    any::{type_name, Any, TypeId},
+    collections::HashMap,
+    marker::PhantomData,
+};
 
+#[derive(Default)]
 pub struct StateMachine<Data> {
-    states: Vec<Box<dyn StateInternal<Data>>>,
+    states: HashMap<TypeId, Box<dyn StateHolder<Data>>>,
+}
+
+impl<D> StateMachine<D> {
+    pub fn add_state<T: State<Data = D>>(&mut self) {
+        self.states
+            .entry(TypeId::of::<T>())
+            .or_insert(Box::new(StateHolderStruct::<T>::default()));
+    }
+    
+    pub fn runner<Start: State>(
+        &self,
+        initial_data: D,
+        start_transition_data: Start::Income,
+    ) -> Option<StateMachineRunner<D>> {
+        StateMachineRunner::new::<Start>(self, initial_data, start_transition_data)
+    }
+
+    fn make_state(&self, state: TypeId) -> Option<Box<dyn StateInternal<D>>> {
+        self.states.get(&state).map(|sh| sh.new())
+    }
+}
+
+pub struct StateMachineRunner<'a, Data> {
+    machine: &'a StateMachine<Data>,
     data: Data,
+    state: Box<dyn StateInternal<Data>>,
+    state_id: TypeId,
+}
+
+pub enum StepOutcome<'a, Data> {
+    Continue {
+        machine: StateMachineRunner<'a, Data>,
+    },
+    Transition {
+        machine: StateMachineRunner<'a, Data>,
+        start: String,
+        transition: String,
+        end: String,
+    },
+    Complete {
+        data: Data,
+        start: String,
+        transition: String,
+    },
+    StateNotFound {
+        start: String,
+        transition: String,
+        end: TypeId,
+    },
+}
+
+impl<'a, D> StateMachineRunner<'a, D> {
+    pub fn new<Start: State>(
+        machine: &'a StateMachine<D>,
+        data: D,
+        start: Start::Income,
+    ) -> Option<Self> {
+        let state_id = TypeId::of::<Start>();
+        let mut state = machine.make_state(state_id)?;
+        state.enter(Box::new(start));
+        Some(Self {
+            machine,
+            data,
+            state,
+            state_id,
+        })
+    }
+
+    pub fn step(mut self) -> StepOutcome<'a, D> {
+        let outcome = self.state.handle(&mut self.data);
+        if outcome.state_type() != self.state_id {
+            let start = self.state.name();
+            let transition = outcome.name();
+            if outcome.state_type() == TypeId::of::<()>() {
+                return StepOutcome::Complete {
+                    data: self.data,
+                    start,
+                    transition,
+                };
+            }
+            self.state_id = outcome.state_type();
+            let Some(state) = self.machine.make_state(self.state_id) else {
+                return StepOutcome::StateNotFound{
+                    start,
+                    transition,
+                    end: outcome.state_type(),
+                };
+            };
+            self.state = state;
+            self.state.enter(outcome.data());
+            let end = self.state.name();
+            return StepOutcome::Transition {
+                machine: self,
+                start,
+                transition,
+                end,
+            };
+        }
+        return StepOutcome::Continue { machine: self };
+    }
+
+    pub fn run_to_completion(mut self) -> Option<D> {
+        loop {
+            self = match self.step() {
+                StepOutcome::Continue { machine } => machine,
+                StepOutcome::Transition {
+                    machine,
+                    start,
+                    transition,
+                    end,
+                } => {
+                    println!("{start} --[{transition}]--> {end}");
+                    machine
+                },
+                StepOutcome::Complete {
+                    data,
+                    start,
+                    transition,
+                } => {
+                    println!("{start} --[{transition}]--> END");
+                    break Some(data);
+                }
+                StepOutcome::StateNotFound {
+                    start,
+                    transition,
+                    end,
+                } => {
+                    println!("{start} --[{transition}]--> {end:?}? ABORT!");
+                    break None;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+struct StateHolderStruct<T: State>(PhantomData<T>);
+
+trait StateHolder<Data> {
+    fn new(&self) -> Box<dyn StateInternal<Data>>;
+}
+
+impl<D, T, I, O> StateHolder<D> for StateHolderStruct<T>
+where
+    T: State<Income = I, Transition = O, Data = D>,
+    I: 'static,
+    O: IntoOutcome + 'static,
+{
+    fn new(&self) -> Box<dyn StateInternal<D>> {
+        Box::new(T::default())
+    }
 }
 
 trait StateInternal<Data> {
     fn enter(&mut self, meta: Box<dyn Any>);
     fn handle(&mut self, data: &mut Data) -> Box<dyn Outcome>;
+    fn name(&self) -> String;
 }
 
 pub trait State: Default + 'static {
-    type Income;
-    type Outcome: Outcome;
+    type Income: 'static;
+    type Transition: IntoOutcome;
     type Data;
 
     fn init(&mut self, previous: Box<Self::Income>);
-    fn handle(&mut self, data: &mut Self::Data) -> Self::Outcome;
+    fn handle(&mut self, data: &mut Self::Data) -> Self::Transition;
+
+    fn name(&self) -> String {
+        type_name::<Self>().to_string()
+    }
 }
 
-impl<T, I, O, Data> StateInternal<Data> for T
+impl<T, I, O, D> StateInternal<D> for T
 where
-    T: State<Income = I, Outcome = O, Data = Data>,
+    T: State<Income = I, Transition = O, Data = D>,
     I: 'static,
-    O: Outcome + 'static,
+    O: IntoOutcome + 'static,
 {
-    fn handle(&mut self, data: &mut Data) -> Box<dyn Outcome> {
-        Box::new(self.handle(data))
+    fn enter(&mut self, meta: Box<dyn Any>) {
+        self.init(
+            meta.downcast()
+                .expect("Expected 'enter' type to match 'income' type"),
+        )
     }
 
-    fn enter(&mut self, meta: Box<dyn Any>) {
-        self.init(meta.downcast().unwrap())
+    fn handle(&mut self, data: &mut D) -> Box<dyn Outcome> {
+        self.handle(data).into_outcome()
+    }
+
+    fn name(&self) -> String {
+        <Self as State>::name(&self)
     }
 }
 
-pub struct OutcomeData<T: State>(T::Income);
+pub struct OutcomeData<T: State>(T::Income, String);
+
+// impl<T> Default for OutcomeData<T>
+// where
+//     T::Income: Default,
+//     T: State,
+// {
+//     fn default() -> Self {
+//         Self(Default::default())
+//     }
+// }
 
 /// If data does not return the incoming data associated with the state corresponding to the result of state_type then the state machine will likely panic
 pub trait Outcome {
     fn state_type(&self) -> TypeId;
-    fn data(self) -> Box<dyn Any>;
+    fn data(self: Box<Self>) -> Box<dyn Any>;
+
+    fn name(&self) -> String {
+        type_name::<Self>().to_string()
+    }
 }
 
-// trait IntoOutcome<T: State> {
-//     fn data(self) -> T::Outcome;
-// }
-
-// impl<S: State, T: IntoOutcome<S>> Outcome for T {
-
-// }
-
-// impl<T: State> Outcome for OutcomeData<T> {
-//     fn state_type(&self) -> TypeId {
-//         TypeId::of::<T>()
-//     }
-
-//     fn data(self) -> Box<dyn Any> {
-//         Box::new(self.0)
-//     }
-// }
-
-pub trait OutcomeInternal {
-    type IntoState: State;
-
-    fn data(self) -> Box<dyn Any>;
+pub trait IntoOutcome {
+    fn into_outcome(self) -> Box<dyn Outcome>;
 }
 
-impl<T> Outcome for T
+impl Outcome for () {
+    fn state_type(&self) -> TypeId {
+        TypeId::of::<()>()
+    }
+
+    fn data(self: Box<Self>) -> Box<dyn Any> {
+        Box::new(())
+    }
+
+    fn name(&self) -> String {
+        "(Complete)".to_string()
+    }
+}
+
+impl<T> OutcomeData<T>
 where
-    T: OutcomeInternal,
+    T: State,
+{
+    pub fn new(data: T::Income) -> OutcomeData<T> {
+        OutcomeData(data, type_name::<T>().to_string())
+    }
+
+    pub fn with_name(data: T::Income, name: String) -> OutcomeData<T> {
+        OutcomeData(data, name)
+    }
+}
+
+impl<T> Outcome for OutcomeData<T>
+where
+    T: State,
 {
     fn state_type(&self) -> TypeId {
-        TypeId::of::<T::IntoState>()
+        TypeId::of::<T>()
     }
 
-    fn data(self) -> Box<dyn Any> {
-        self.data()
+    fn data(self: Box<Self>) -> Box<dyn Any> {
+        Box::new(self.0)
+    }
+
+    fn name(&self) -> String {
+        self.1.clone()
     }
 }
 
-impl<T: State> OutcomeInternal for OutcomeData<T> {
-    type IntoState = T;
-
-    fn data(self) -> Box<dyn Any> {
-        Box::new(self.0)
+impl<T> IntoOutcome for T
+where
+    T: Outcome + 'static,
+{
+    fn into_outcome(self) -> Box<dyn Outcome> {
+        Box::new(self)
     }
 }
